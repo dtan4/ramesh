5ae7ba60a35490e0b508fecbf8ba7d59e02ac96b
dtan4 <dtanshi45@gmail.com>
2014-08-11 18:47:28 +0900

    Allow connection to Code Climate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,8 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'ramesh'
 require 'webmock/rspec'
 
+WebMock.disable_net_connect!(allow: "codeclimate.com")
+
 def fixture_path(name)
   File.expand_path(File.join("..", "fixtures", name), __FILE__)
 end
